c088a81230aaab8331d0bfd5f6642e93473bd4d4
S-Swaroop <itsmesharan2000@gmail.com>
2021-07-27 11:04:03 +0530

    created basic routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import DashBoard from './components/DashBoard/DashBoard'
+
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div>
+          <nav className="nav navbar-nav">
+            <ul className="nav nav-pills nav-fill">
+              <li className="nav-item">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/login">DashBoard</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/signup">Sign up</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/dashnoard">login</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Switch >
+          <Route path="/" exact> <Home /></Route>
+          <Route path="/login" exact> <Login /></Route>
+          <Route path="/signup" exact> <SignUp /></Route>
+          <Route path="/dashboard" exact> <DashBoard /></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
