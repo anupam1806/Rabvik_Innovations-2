8b46ff15a3dfc59e23c51a753174b806d403d47b
S-Swaroop <itsmesharan2000@gmail.com>
2021-08-28 16:08:04 +0530

    added financials schema in models

---
+++ models/FinancialsData.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose');
+
+const FinancialsSchema = new mongoose.Schema({
+    Revenues : {
+        type : [Number] 
+    },
+    'Cost Of Goods' : {
+        type : [Number] 
+    },
+    Salaries : {
+        type : [Number] 
+    },
+    "Operation Cost" : {
+        type : [Number] 
+    },
+    EBITDA : {
+        type : [Number] 
+    },
+    "D&A" : {
+        type : [Number]
+    },
+    EBIT : {
+        type : [Number]
+    },
+    Interest : {
+        type : [Number]
+    },
+    Taxes : {
+        type : [Number]
+    },
+    "Net Profit" : {
+        type : [Number]
+    },
+    Recieveables : {
+        type : [Number]
+    },
+    Inventory : {
+        type : [Number]
+    },
+    Payables : {
+        type : [Number]
+    },
+    "Working Capital" : {
+        type : [Number]
+    },
+    "Capital Expenditures" : {
+        type : [Number]
+    },
+    "Debt at the end of the year" : {
+        type : [Number]
+    },
+    "Change in outstanding debt" : {
+        type : [Number]
+    },
+    "Future Funding" : {
+        type : [Number]
+    },
+    "Free cash flow" : {
+        type : [Number]
+    },
+    "Cash and equivalents" : {
+        type : [Number]
+    },
+    "Tangible Assets" : {
+        type : [Number]
+    },
+    "Financial Assets" : {
+        type : [Number]
+    },
+    "Differed Tax Assets" : {
+        type : [Number]
+    },
+    "Debts due within one year time" : {
+        type : [Number]
+    },
+    "Debts due beyond one year time" : {
+        type : [Number]
+    },
+    "Equity" : {
+        type : [Number]
+    }
+});
+
+module.exports = mongoose.model('financials', FinancialsSchema);
