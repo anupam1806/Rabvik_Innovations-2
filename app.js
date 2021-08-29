0f14e46126e5be750b7954553de18ed97767dca8
Anupam <anupam.saini18@gmail.com>
2021-08-29 22:43:24 +0530

    Port

--- app.js
+++ app.js
@@ -277,7 +277,6 @@ app.get("/logout", function(req, res){
 
 
 
-app.listen(process.env.PORT || 8000, function(){
-
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
