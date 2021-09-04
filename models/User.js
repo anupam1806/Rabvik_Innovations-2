a3357caba0d7d0aceeb0c91f78bbdc9286272b35
Sarvesh Prajapati <thesarveshprajapati@hotmail.com>
2021-09-04 17:33:46 +0530

    made code a bit cleaner

--- models/User.js
+++ models/User.js
@@ -1,6 +1,32 @@
 const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-  });
-module.exports = mongoose.model("user", userSchema);
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String
+    },
+    password: {
+        type:String
+    },
+    googleId:{
+        type:String
+    },
+    companyName:{
+        type:String
+    },
+    email:{
+        type:String
+    },
+    firstName:{
+        type:String
+    },
+    lastName:{
+        type:String
+    },
+    image:{
+        type:String
+    },
+    issuedAt:{
+        type:Date,
+        default:Date.now
+    }
+});
+module.exports = mongoose.model("user", UserSchema);
